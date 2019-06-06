b973cd758b40f67fe26f5f21df74a7b3c172925a
chrisHuxi <chris_huxi@163.com>
2019-06-06 15:17:09 +0200

    vis

--- scheduler.py
+++ scheduler.py
@@ -1,6 +1,9 @@
 from typing import List, Optional, Dict
 from collections import deque
 
+import matplotlib.pyplot as plt
+import numpy as np
+
 # =====Helper functions=============
 def head(l: list):
     """
@@ -16,7 +19,7 @@ class Operation:
         self.machine = machine
         self.time = time
 
-    def __repr__(self):
+    def __repr__(self): #这个方法是用来display的
         return "<{};{};{}>".format(self.job, self.index, self.machine)
     
     def to_timespan(self, start: int):
@@ -192,85 +195,83 @@ class HillClimbingOptimizer():
             else:
                 current_ts = next_ts
 
-# each row is job
-# each colum is machine : time
-def readData(file_name) -> List[Operation]:
-    with open('./data/'+file_name, 'r') as f:
+    
+class Visualizator():
+    def __init__(self, scheduleDict, total_time):
+        self.scheduleDict = scheduleDict
+        self.total_time = total_time
         
-        lines = f.readlines()
-        job_number = int(lines[0].strip().split(' ')[0])
-        machine_number = int(lines[0].strip().split(' ')[1])
-        
-        operation_list = []
-        for i in range(0,len(lines[1:])):
-            time_list = lines[i+1].strip().split(' ')
-            for j in range(0,len(time_list),2):
-                operation_list.append(Operation(i, int(j/2), int(time_list[j]), int(time_list[j+1])))
-    return operation_list
-        
-    
-def arrange(operation_list):
-    #TODO: how to generate a new operation_list? ==> simulated annealing
-    
-    return operation_list
-    
-    
-def evaluate(operation_list):
-    legal_flag = False
-    #TODO:
-    #firstly check if this list legal or not
-    # but how? ==> main idea: if there is no circle: legal
-    # is there a tool or algorithm to check circle?
-    for i in range(len(operation_list)):
-        op1 = operation_list[i]
-        if i < len(operation_list) - 1:
-            op2 = operation_list[i+1]
-        
-    
-    
-    
-    if legal_flag == True:
-        #then check the total time of this operation_list
-        total_time = calculateTotalTime(operation_list)
-        return total_time
-    else:
-        print("this arrangement is illegal!")
-        return 9999
+    def avg(self, a, b):
+        return (a + b) / 2.0
+            
+    def plot(self):
+        plot_list = []
+        for machine_idx in self.scheduleDict.keys():
+            time_line = [-1]*self.total_time
 
-#input: {0: [op1, op3, ...]; 1: [op0, op2, ...]}
-def calculateTotalTime(machine_to_op_dict):
-    total_time_list = []
-    for machine in machine_to_op_dict.keys():
-        total_time = 0
-        op_list = machine_to_op_dict[machine]
-        for op in op_list:
-            total_time += op.time
-        total_time_list.append(total_time)
-    print("max total time of this arrangement:")
-    print(max(total_time_list))
-    return max(total_time_list)
-    
+            for span in self.scheduleDict[machine_idx]:
+                #time_line[span.start:span.end] = span.operation.job
+                for i in range(span.start,span.end):
+                    time_line[i] = span.operation.job
+            plot_list.append(time_line)
+        print(plot_list[4])
+        fig = plt.figure(figsize=(20, 6))
+        ax = fig.add_subplot(111)
+        ax.axes.get_yaxis().set_visible(False)
+        #ax.set_aspect(1)
+        
+        for y, row in enumerate(plot_list):
+            y1 = np.array([y, y])
+            y2 = y1+1
+            current_span_len = 0
+            current_job_idx = -1
+            empty_flag = True
+            x_start = 0
+            for x, col in enumerate(row):
+                if (col == -1) and (current_span_len == 0): 
+                    empty_flag = True
+                    continue
+                elif col != -1:
+                    if empty_flag == True: # 上边缘
+                        x_start = x
+                        empty_flag = False
+                        current_job_idx = col
+                    else:                    
+                        current_span_len += 1
+                elif (col == -1) and (current_span_len!=0): #下边缘
+                    x1 = np.array([x_start, (x_start + current_span_len)])
+                    plt.fill_between(x1, y1, y2, color=( current_job_idx/10.0, 0.5, 0.8))
+                    plt.text(self.avg(x1[0], x1[1]), self.avg(y1[0], y2[0]), "job" + str(current_job_idx), 
+                                                    horizontalalignment='center',
+                                                    verticalalignment='center')
+                    current_span_len = 0
+                else:
+                    print("---------------------")
+        plt.ylim(10,0)
+        plt.xlim(0,self.total_time)
+        
+        plt.show()
+        
     
 def main():
-    # initial_operation_list = readData('test_data1.txt')
-    # new_operation_list = arrange(initial_operation_list)
-    # evaluate(new_operation_list)
-
     
     ts = TopologicalSort.read_from_file('test_data1.txt')
-    print(ts)
-    print("Score: {}".format(ts.make_span()))
-    opt = HillClimbingOptimizer.optimize(ts)
-    print(opt)
-    print("Score: {}".format(opt.make_span()))
+    #print(ts.opList)
+    print(ts.get_schedule())
+    #print(ts.get_schedule().scheduleDict)
+    #print(ts.make_span())
+    test_v = Visualizator(ts.get_schedule().scheduleDict, ts.make_span())
+    test_v.plot()
+    #print("Score: {}".format(ts.make_span()))
+    #opt = HillClimbingOptimizer.optimize(ts)
+    #print(opt)
+    #print("Score: {}".format(opt.make_span()))
     
 if __name__ == "__main__":
     main()
-
-
-example_topSort = TopologicalSort(2,[
-    Operation(0,0,0,80),
-    Operation(0,1,1,60),
-    Operation(1,0,0,50),
-    Operation(1,1,1,100),
-])
+    example_topSort = TopologicalSort(2,[
+        Operation(0,0,0,80),
+        Operation(0,1,1,60),
+        Operation(1,0,0,50),
+        Operation(1,1,1,100),
+    ])
